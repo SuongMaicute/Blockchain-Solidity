0279ea34ed87215364c8672a90e34fa8bb6128aa
Mai Đinh Thị Sương <101706623+SuongMaicute@users.noreply.github.com>
2023-06-06 19:42:15 +0700

    Create interface.sol

---
+++ interface.sol
@@ -0,0 +1,48 @@
+
+pragma solidity ^0.8.0;
+
+abstract contract ReentrancyGuard {
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor () {
+        _status = _NOT_ENTERED;
+    }
+
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+        _status = _NOT_ENTERED;
+    }
+}
+
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+ 
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+  
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
